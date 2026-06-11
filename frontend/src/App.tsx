47d2dfd87c8dac04d4cfebbf6bd61ea8d3ec793f
Thomas Griffin <tjgriffin@proton.me>
2026-06-11 10:48:35 -0400

    Resizable editor and preview with a 50% snap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,35 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 import './App.css'
 import Editor from './components/Editor'
 import Preview from './components/Preview'
+import { Group, Panel, Separator, type Layout, type PanelImperativeHandle } from 'react-resizable-panels';
 
 function App() {
   const [content, setContent] = useState('');
+  const panelRef = useRef<PanelImperativeHandle>(null);
+
+  function handleLayoutChanged(layout: Layout){
+    const editorSize = layout["editor"];
+    if (editorSize !== 50 &&editorSize > 48 && editorSize < 52) {
+      panelRef.current?.resize("50%");
+    }
+  }
 
   return (
     <>
       <h1 className="text-2xl font-bold mb-4">My App</h1>
       
-      <div className="flex flex-row w-full h-screen">
-        <Editor value={content} onChange={setContent} />
-        <Preview content={content} />
+      <div className="flex flex-col w-full h-screen">
+        <Group orientation="horizontal" onLayoutChange={handleLayoutChanged}>
+          <Panel panelRef={panelRef} id="editor" defaultSize="50%" minSize="30%">
+            <Editor value={content} onChange={setContent} />
+          </Panel>
+          <Separator />
+          <Panel id="preview" defaultSize="50%" minSize="30%">
+            <Preview content={content} />
+          </Panel>
+        </Group>
       </div>
     </>
   )
